2945b4f62aff1fb97529af302b37b83b1b91a358
Hugo <hugobillemartins@gmail.com>
2022-09-29 14:44:19 -0300

    hi

--- natal/admin.py
+++ natal/admin.py
@@ -4,5 +4,5 @@ from .models import *
 # Register your models here.
 admin.site.register(Parceiro)
 admin.site.register(Evento)
-admin.site.register(Testemonio)
+admin.site.register(Testemunho)
 admin.site.register(Galeria)
