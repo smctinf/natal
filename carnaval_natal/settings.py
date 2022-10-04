9773c5761435364e03c72a9c86a35a1820172b08
Hugo <hugobillemartins@gmail.com>
2022-10-04 14:28:04 -0300

    luis falando minha senha super segura'

--- carnaval_natal/settings.py
+++ carnaval_natal/settings.py
@@ -108,7 +108,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'natal/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
 
 LOGIN_URL='/s/login'
 LOGIN_REDIRECT_URL = '/'
