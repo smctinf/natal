05dbb49592ae7103542214ea579e1b164d4d9d97
Hugo <hugobillemartins@gmail.com>
2022-10-04 15:12:15 -0300

    luis falando minha senha super segura

--- carnaval_natal/urls.py
+++ carnaval_natal/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('core.urls')),
+    path('/carnaval', include('core.urls')),
     path('admin/', admin.site.urls),
     path('', include('natal.urls')),
 ]
