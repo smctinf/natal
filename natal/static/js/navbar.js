f6470b7446aa969ca436353c5b242930ab5c1f54
Hugo <hugobillemartins@gmail.com>
2022-11-29 15:56:47 -0300

    hu

--- natal/static/js/navbar.js
+++ natal/static/js/navbar.js
@@ -1,11 +1,21 @@
 let isClosed = true
 const hamburger = document.querySelector(".navbar-toggler")
+const navbar = document.querySelector(".navbar-collapse")
+const nav = document.querySelector('nav')
+const logo = document.querySelector('#logoFriburgo')
+hamburger.className = "navbar-toggler text-white"
+navbar.style.display = "none"
+nav.className = "navbar navbar-expand-lg" 
+logo.style.display = 'block'
+
+console.log(isClosed)
+console.log(navbar)
+
+
 hamburger.addEventListener('click', ()=>{
     isClosed = !isClosed
     hamburger.className = isClosed ? "navbar-toggler text-white" : "navbar-toggler text-white spin"
-    const navbar = document.querySelector(".navbar-collapse")
     navbar.style.display = isClosed ? "none" : "block"
-    document.querySelector('nav').className = isClosed ? "navbar navbar-expand-lg" : "navbar navbar-expand-lg showingToo"
+    nav.className = isClosed ? "navbar navbar-expand-lg" : "navbar navbar-expand-lg showingToo"
     navbar.className = isClosed ? "collapse navbar-collapse" : "collapse navbar-collapse showing"
-    document.querySelector('#logoFriburgo').style.display = isClosed ? 'block' : 'none'
 })
