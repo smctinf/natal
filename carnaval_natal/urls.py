39ed847fe8f734e40f599bbb8884b1a86ac14177
Eduardo Salarini <luiseduardo@id.uff.br>
2023-01-18 11:43:07 -0300

    att

--- carnaval_natal/urls.py
+++ carnaval_natal/urls.py
@@ -7,9 +7,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('carnaval', include('core.urls')),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
-    path('', include('natal.urls')),
+    path('natal', include('natal.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
